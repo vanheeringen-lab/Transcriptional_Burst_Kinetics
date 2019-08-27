1304ee92c323d2790618cdcd2022b0feab4e5a8b
Maarten van der Sande <maartenvandersande@hotmail.com>
2019-08-27 16:20:30 +0200

    implemented moment inference method

--- MMGPS/main.py
+++ MMGPS/main.py
@@ -1,5 +1,7 @@
 import simpy
 from gene import Gene
+from moment_inference import moment_inference
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -8,15 +10,20 @@ if __name__ == "__main__":
     nu = 4       # rate from active -> product
     delta = 1    # rate from product -> degraded
 
-    # start the environment
-    env = simpy.Environment()
+    products = []
+    for i in range(150):
+        # start the environment
+        env = simpy.Environment()
 
-    # make a gene
-    gene = Gene(env, lambd, mu, nu, delta)
+        # make a gene
+        gene = Gene(env, lambd, mu, nu, delta)
 
-    # run for fixed amount of time
-    env.run(until=5000)
+        # run for fixed amount of time
+        env.run(until=3000)
 
-    products = [product for product in gene.products if not product.degraded]
-    print(f"There are {len(products)} gene products after {env.now} gene-product half-times,")
-    print(f"and we expect: {((lambd * nu) / ((lambd + mu) * delta))}")
+        products.append(len([product for product in gene.products if not product.degraded]))
+
+    print(f'The parameters are: lambda {lambd}, mu {mu}, nu {nu}, delta {delta}')
+    print(f'the parameters based on moment inference is: {moment_inference(np.array(products))}')
+    print(f"The average of gene-products after {env.now} gene-product half-times is {np.mean(products)},")
+    print(f"And theoretically we expect: {((lambd * nu) / ((lambd + mu) * delta))}")
